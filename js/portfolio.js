3b38eb850ed35745941d6ba66224e750a783e3ab
Daniel Hoang <thesubway@gmail.com>
2016-01-21 12:07:06 -0800

    Use Swipebox to showcase images

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,15 @@
-$('#gallery').justifiedGallery();
+
+$('#gallery').rowHeight = 4;
+$( document ).ready(function() {
+	var gallery = $('#gallery').justifiedGallery({
+		rowHeight: 200,
+		lastRow: 'justify',
+		margins: 3,
+		randomize: true
+	});
+
+	gallery.on('jg.complete', function () {
+        alert('the gallery is complete');
+        $('#gallery a').swipebox({hideBarsDelay: false});
+    });
+});
