893c08da1dbba08d889d8c467e9a898e43c30fd2
Daniel Hoang <thesubway@gmail.com>
2016-01-21 16:35:20 -0800

    Set contact to send email using formspree

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {
 	$('form').submit(function(event){
-		alert('button clicked');
-	})
+		event.preventDefault();
+		$.ajax({
+    url: "//formspree.io/danielhoang1@me.com", 
+    method: "POST",
+    data: {
+    	email: $('js-email').val(),
+    	message: $('#js-message').val(),},
+    dataType: "json"
+	}).error(function(jqXHR, status, error){
+        alert('Could not send, something went wrong. Please try again.');
+    }).success(function(data, status, jqXHR) {
+            alert('Message sent! Dan will get back to you soon.');
+            $('form').find('#js-email, #js-message').val('');
+        });;
+	});
 });
